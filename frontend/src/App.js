5271f703d32c6860eb17bf567b6712313cb61bfe
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-06-09 02:35:06 +0530

    added home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Stars from "./components/particles";
 
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='flex items-center justify-center'>
+      <h1 className='text-white font-bold text-[30px]'>hello</h1>
+      <Stars/>
+    </div>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors:{
+        'primary-500': '#fe7201',
+        'primary-400': '#fe7e17',
+        'primary-300': '#fe8a2d',
+        'primary-200': '#fe9743',
+        'primary-100': '#fea359',
+        'secondary': '#f9f9f9'
+      }
+    },
   },
   plugins: [],
 }
