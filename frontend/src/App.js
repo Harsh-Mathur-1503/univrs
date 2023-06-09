2cdebf1bb41fdea8a54815eb4b02ea18c92892f5
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-06-09 11:39:05 +0530

    added tsparticles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Stars from "./components/particles";
+import Home from './pages/Home';
+
 
 const App = () => {
   return (
-    <div className='flex items-center justify-center'>
-      <h1 className='text-white font-bold text-[30px]'>hello</h1>
+    <div className='app'>
+      <Home/>
       <Stars/>
     </div>
   )
---
+++ frontend/src/components/particles.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./particles.css";
+
+export default function Stars() {
+  return (
+    <div>
+      <section class="wrapper">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        <div id="title">
+            <span>PARALLAX PIXEL BACKGROUND</span>
+        </div>
+    </section>
+    </div>
+  );
+}
