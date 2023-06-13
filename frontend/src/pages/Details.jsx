d62c70b5e9216a2063aa1389225e6b2d304a4cee
lonewolf1503 <104578544+lonewolf1503@users.noreply.github.com>
2023-06-13 11:01:10 +0530

    fixed a local bug

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -5,10 +5,10 @@ import details_img from '../assets/details_1.jpg'
 
 const Details = () => {
   return (
-    <div className='detailss h-[100vh] w-5/6 mx-auto flex flex-row items-center justify-between'>
+    <div className='detailss h-[100vh] w-5/6 mx-auto flex flex-row items-center justify-between relative'>
         <img src={details_img} alt="image"  className='w-1/3 h-[60vh] rounded-2xl'/>
         <div>
-            dslfjsdklfjlskjf
+            Something
         </div>
     </div>
   )
